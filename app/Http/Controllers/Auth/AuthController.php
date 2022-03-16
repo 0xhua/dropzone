c5db8e1b77c96f4ec0d5a3251372d000a98a5800
Joshua <ohua29@gmail.com>
2022-03-16 22:30:27 +0800

    add fail messages to login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,27 +5,35 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function authenticate(Request $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
-        if (Auth::attempt($credentials)) {
+        if($validator->fails()){
+            return Redirect::to(route('home'). "#login")->withErrors($validator);
+        }
+        $validator = $validator->validated();
+
+        if (Auth::attempt($validator)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('dashboard');
+            return redirect()->intended('seller_dashboard');
         }
 
-        return back()->withErrors([
+        return Redirect::to(route('home'). "#login")->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
     }
-    
+
     public function logout(Request $request)
     {
         Auth::logout();
