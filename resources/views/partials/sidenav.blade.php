db2521a862c3f7caa107a705bc7e9ef9ea011603
Joshua <ohua29@gmail.com>
2022-03-16 22:32:45 +0800

    add nav blade

---
+++ resources/views/partials/sidenav.blade.php
@@ -0,0 +1,55 @@
+<div class="l-navbar" id="nav-bar">
+    <nav class="nav">
+        <div>
+            <a href="#" class="nav_logo">
+                <img src="{{asset('images/profile.png')}}" class="header_img" alt="profile">
+                <lable id="uname">Username</lable>
+                <br>
+                <lable id="role">Seller's Location</lable>
+            </a>
+
+
+            <div class="nav_list">
+                <a href="{{route('seller_dashboard')}}"
+                   class="nav_link {{request()->is('seller_dashboard')?'active':''}}">
+                    <i class='bx bxs-dashboard nav_icon'></i>
+                    <span class="nav_name">Dashboard</span>
+                </a>
+
+                <a href="{{route('seller_itemlist')}}"
+                   class="nav_link {{request()->is('seller_itemlist')?'active':''}}">
+                    <i class='bx bx-list-ul nav_icon'></i>
+                    <span class="nav_name">Item List</span>
+                </a>
+
+                <a href="seller_request.html" class="nav_link">
+                    <i class="bx bxs-message-alt-add nav_icon"></i>
+                    <span class="nav_name">Request</span>
+                </a>
+
+                <a href="seller_updates.html" class="nav_link">
+                    <i class='bx bxs-message-alt-check nav_icon'></i>
+                    <span class="nav_name">Updates Tab</span>
+                </a>
+
+                <a href="seller_tutor.html" class="nav_link">
+                    <i class='bx bx-book-bookmark nav_icon '></i>
+                    <span class="nav_name">Tutorial</span>
+                </a>
+
+                <a href="seller_setting.html" class="nav_link">
+                    <i class='bx bxs-cog nav_icon'></i>
+                    <span class="nav_name">Account Setting</span>
+                </a>
+            </div>
+
+
+        </div>
+
+        <a href="{{route('logout')}}" class="nav_link">
+            <i class='bx bx-log-out nav_icon'></i>
+            <span class="nav_name">LOGOUT</span>
+        </a>
+
+    </nav>
+</div>
---
+++ resources/views/partials/topnav.blade.php
@@ -0,0 +1,13 @@
+<header class="header" id="header">
+
+
+    <div class="col-sm-11" style="height: 50px; margin-top: 15px;">
+        <i class='bx bx-menu' id="header-toggle"></i>
+    </div>
+
+    <div class="col-sm-1" style="height: 50px;">
+        <img src="images/logo.png" class="logo" alt="">
+    </div>
+
+
+</header>
