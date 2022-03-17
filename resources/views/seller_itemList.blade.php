b816e6af3788c6d5b6579a8fc6b52eb97e63b68b
Joshua <ohua29@gmail.com>
2022-03-18 01:00:55 +0800

    fix seller_itemList.blade.php

--- resources/views/seller_itemList.blade.php
+++ resources/views/seller_itemList.blade.php
@@ -36,22 +36,19 @@
                         </div>
 
                         <!-- Modal body -->
-                        <form>
+                        <form method="post" action="{{route('add-item')}}">
                             <div class="modal-body">
                                 <div class="row" style="margin-left: 5px;">
                                     <div class="col-sm-6 mb-3">
+                                        {{csrf_field()}}
+                                        <label id="seller_id">Seller</label><br>
+                                        <input type="text" placeholder="123" name="seller_id"><br>
 
-                                        <label id="">Date</label><br>
-                                        <input type="date" placeholder=""><br>
-
-                                        <label id="">Seller</label><br>
-                                        <input type="text" placeholder=""><br>
-
-                                        <label id="">Buyer</label><br>
-                                        <input type="text" placeholder=""><br>
+                                        <label id="buyer">Buyer</label><br>
+                                        <input type="text" placeholder="" name="buyer_id"><br>
 
                                         <label id="">Paid</label>
-                                        <select value="" name="paid" id="paid">
+                                        <select value="" name="payment_status_id" id="paid">
                                             <option disabled selected value> -- paid/unpaid --</option>
                                             @foreach($paid_statuses as $status)
                                                 <option value="{{$status->id}}">{{$status->status}}</option>
@@ -62,7 +59,7 @@
 
                                     <div class="col-sm-6 mb-3">
                                         <label id="">Origin</label><br>
-                                        <select value="" name="origin" id="itemOrigin">
+                                        <select value="" name="origin_id" id="itemOrigin">
                                             <option disabled selected value> -- select origin --</option>
                                             @foreach($location as $area)
                                                 <option value="{{$area->id}}">{{$area->area}}</option>
@@ -71,7 +68,7 @@
 
 
                                         <label id="" style="margin-top: 20px;">Destination </label><br>
-                                        <select value="" name="destination" id="itemDesti">
+                                        <select value="" name="destination_id" id="itemDesti">
                                             <option disabled selected value> -- select destination --</option>
                                             @foreach($location as $area)
                                                 <option value="{{$area->id}}">{{$area->area}}</option>
@@ -88,7 +85,7 @@
                                         <br>
 
                                         <label id="" style="margin-top: 20px;"> Item Amount</label><br>
-                                        <input type="text" placeholder="">
+                                        <input type="text" placeholder="₱100.00" name="amount">
                                     </div>
 
                                 </div>
@@ -98,7 +95,7 @@
                                     <button type="button" class="btn btn-danger" data-dismiss="modal">Discard
                                     </button>
 
-                                    <button type="button" class="btn btn-success" data-dismiss="modal">Create
+                                    <button type="submit" class="btn btn-success" >Create
                                     </button>
                                 </div>
                             </div>
@@ -136,7 +133,6 @@
     </div>
     </div>
 
-    <!------------------------ TABLE ------------------------>
     <div class="row" style="margin-top: 2%; color: white;">
         <div class="table-responsive">
             <table class="table table-responsive table-border">
@@ -161,6 +157,105 @@
                     </thead>
 
                     <tbody id="myTable">
+                    @foreach($items as $item)
+                        <tr>
+                            <td>
+                                @if($item->code)
+                                    {{$item->code}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->drop_date)
+                                    {{$item->drop_date}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->seller)
+                                    {{$item->seller}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->buyer)
+                                    {{$item->buyer}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->origin)
+                                    {{$item->origin}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->destination)
+                                    {{$item->destination}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->fee)
+                                    {{$item->fee}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->amount)
+                                    {{$item->amount}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->payment_status)
+                                    {{$item->payment_status}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                @if($item->status)
+                                    {{$item->status}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+{{--                                @if($item->claimed_date)--}}
+{{--                                    {{$item->claimed_date}}--}}
+{{--                                @else--}}
+                                    N/A
+{{--                                @endif--}}
+                            </td>
+                            <td>
+{{--                                @if($item->release_date)--}}
+{{--                                    {{$item->release_date}}--}}
+{{--                                @else--}}
+                                    N/A
+{{--                                @endif--}}
+                            </td>
+                            <td>
+                                @if($item->approval_status)
+                                    {{$item->approval_status}}
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td>
+                                <i class='bx bx-qr-scan' style="font-size: 24px;" data-toggle="modal"
+                                   data-target="#generateQr"></i>
+                            </td>
+                        </tr>
+                    @endforeach
                     <tr>
                         <td>KDS-253</td>
                         <td>03-18-2022</td>
@@ -223,45 +318,5 @@
             });
         });
     </script>
-    <script>
-        document.addEventListener("DOMContentLoaded", function (event) {
-
-            const showNavbar = (toggleId, navId, bodyId, headerId) => {
-                const toggle = document.getElementById(toggleId),
-                    nav = document.getElementById(navId),
-                    bodypd = document.getElementById(bodyId),
-                    headerpd = document.getElementById(headerId)
-
-                // Validate that all variables exist
-                if (toggle && nav && bodypd && headerpd) {
-                    toggle.addEventListener('click', () => {
-                        // show navbar
-                        nav.classList.toggle('show')
-                        // change icon
-                        toggle.classList.toggle('bx-x')
-                        // add padding to body
-                        bodypd.classList.toggle('body-pd')
-                        // add padding to header
-                        headerpd.classList.toggle('body-pd')
-                    })
-                }
-            }
-
-            showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
-
-            /*===== LINK ACTIVE =====*/
-            const linkColor = document.querySelectorAll('.nav_link')
-
-            function colorLink() {
-                if (linkColor) {
-                    linkColor.forEach(l => l.classList.remove('active'))
-                    this.classList.add('active')
-                }
-            }
-
-            linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-        });
-
-    </script>
+    @parent
 @endsection
