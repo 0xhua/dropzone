51fc3804a527945fbd53993013a7c3b05c79714a
Joshua Lictan <60700937+0xhua@users.noreply.github.com>
2022-04-20 13:14:17 +0800

    Run all seeders in seeder folder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,27 +4,40 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+//class DatabaseSeeder extends Seeder
+//{
+//    /**
+//     * Seed the application's database.
+//     *
+//     * @return void
+//     */
+//    public function run()
+//    {
+//        $this->call([
+//            amenItemTableNewIdColumnSeeder::class,
+//            approvalStatusSeeder::class,
+//            ItemSizeSeeder::class,
+//            itemStatusSeeder::class,
+//            locationSeeder::class,
+//            locationTownCodeSeeder::class,
+//            paidStatusSeeder::class,
+//            PermissionTableSeeder::class,
+//            transactionFeeSeeder::class,
+//            CreateAdminUserSeeder::class,
+//            request_catergory_seeder::class
+//        ]);
+//    }
+//}
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call([
-            amenItemTableNewIdColumnSeeder::class,
-            approvalStatusSeeder::class,
-            ItemSizeSeeder::class,
-            itemStatusSeeder::class,
-            locationSeeder::class,
-            locationTownCodeSeeder::class,
-            paidStatusSeeder::class,
-            PermissionTableSeeder::class,
-            transactionFeeSeeder::class,
-            CreateAdminUserSeeder::class,
-            request_catergory_seeder::class
-        ]);
+
+    public function run(){
+        $files_arr = scandir( dirname(__FILE__) ); //store filenames into $files_array
+        foreach ($files_arr as $key => $file){
+            if ($file !== 'DatabaseSeeder.php' && $file[0] !== "." ){
+                $this->call( explode('.', $file)[0] );
+            }
+        }
     }
+
 }
