a4df8d162e7d2d9b2678f4db98beea7bd3cde4e6
Joshua Lictan <60700937+0xhua@users.noreply.github.com>
2022-04-20 13:25:02 +0800

    Run all seeders in seeder folder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            PermissionTableSeeder::class,
             addDaPermissions::class,
             addGenerateQrPermission::class,
             amenItemTableNewIdColumnSeeder::class,
@@ -27,7 +28,6 @@ class DatabaseSeeder extends Seeder
             locationSeeder::class,
             locationTownCodeSeeder::class,
             paidStatusSeeder::class,
-            PermissionTableSeeder::class,
             request_catergory_seeder::class,
             transactionFeeSeeder::class,
         ]);
