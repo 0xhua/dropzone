c349af82be439c9b1b60ebb69174f3d22252341f
Joshua Lictan <60700937+0xhua@users.noreply.github.com>
2022-04-21 18:09:44 +0800

    added home pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,29 +13,7 @@
 
 
 <body id="bootstrap-overrides">
-<header class="header">
-    <nav class="navbar navbar-style">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <img class="logo" src="{{asset('images/logo.png')}}">
-            </div>
-            <div class="collapse navbar-collapse" id="micon">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a class="active" href="front.html">Home</a></li>
-                    <li><a href="branches.blade.php">Branches</a></li>
-                    <li><a href="transfer.blade.php">Transfer Schedule</a></li>
-                    <li><a href="rules.blade.php">Rules</a></li>
-                    <li><a href="contact.blade.php">Contact Us </a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+@include('partials.home.nav')
     <div class="container-fluid">
         <div class="row" id="bgonly">
             <div class="col-md-6">
@@ -200,7 +178,7 @@
         </div>
 
     </div>
-</header>
+
 
 
 </body>
