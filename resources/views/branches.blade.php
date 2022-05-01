72ca3c564effda50aadb5b8714123844ce4e2376
Joshua Lictan <60700937+0xhua@users.noreply.github.com>
2022-05-01 20:34:24 +0800

    FIX BRANCHES

--- resources/views/branches.blade.php
+++ resources/views/branches.blade.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-sm-3" style="text-align: center;">
                 <a class="lightbox" href="#branch">
-                    <img src="{{asset('images/da10.png')}}">
+                    <img src="{{asset('images/da10.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> San Fernando </p>
@@ -23,7 +23,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch1">
-                    <img src="{{asset('images/da2.png')}}">
+                    <img src="{{asset('images/da2.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Bauang </p>
@@ -33,7 +33,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch2">
-                    <img src="{{asset('images/da11.png')}}">
+                    <img src="{{asset('images/da11.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Bacnotan </p>
@@ -43,7 +43,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch3">
-                    <img src="{{asset('images/da8.png')}}">
+                    <img src="{{asset('images/da8.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Agoo </p>
@@ -57,7 +57,7 @@
         <div class="row" id="branches">
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch4">
-                    <img src="{{asset('images/da9.png')}}">
+                    <img src="{{asset('images/da9.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Naguilian</p>
@@ -68,7 +68,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch5">
-                    <img src="{{asset('images/da3.png')}}">
+                    <img src="{{asset('images/da3.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Caba </p>
@@ -78,7 +78,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch6">
-                    <img src="{{asset('images/da4.png')}}">
+                    <img src="{{asset('images/da4.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Aringay </p>
@@ -88,7 +88,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch7">
-                    <img src="{{asset('images/da13.png')}}">
+                    <img src="{{asset('images/da13.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> San Juan </p>
@@ -101,7 +101,7 @@
         <div class="row" id="branches">
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch8">
-                    <img src="{{asset('images/da1.png')}}">
+                    <img src="{{asset('images/da1.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Balaoan</p>
@@ -111,7 +111,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch9">
-                    <img src="{{asset('images/da14.png')}}">
+                    <img src="{{asset('images/da14.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Luna</p>
@@ -121,7 +121,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch10">
-                    <img src="{{asset('images/da15.png')}}">
+                    <img src="{{asset('images/da15.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Bangar </p>
@@ -131,7 +131,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch11">
-                    <img src="{{asset('images/da7.png')}}">
+                    <img src="{{asset('images/da7.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Santo Tomas </p>
@@ -144,7 +144,7 @@
         <div class="row" id="branches">
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch12">
-                    <img src="{{asset('images/da5.png')}}">
+                    <img src="{{asset('images/da5.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Pugo</p>
@@ -154,7 +154,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch13">
-                    <img src="{{asset('images/da17.png')}}">
+                    <img src="{{asset('images/da17.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Tubao</p>
@@ -164,7 +164,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch14">
-                    <img src="{{asset('images/da6.png')}}">
+                    <img src="{{asset('images/da6.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Damortis </p>
@@ -174,7 +174,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch15">
-                    <img src="{{asset('images/da16.png')}}">
+                    <img src="{{asset('images/da16.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Rosario </p>
@@ -187,7 +187,7 @@
         <div class="row" id="branches">
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch16">
-                    <img src="{{asset('images/da12.png')}}">
+                    <img src="{{asset('images/da12.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> San Gabriel</p>
@@ -197,7 +197,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch17">
-                    <img src="{{asset('images/da_loc.png')}}">
+                    <img src="{{asset('images/da_loc.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Baguio</p>
@@ -207,7 +207,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch18">
-                    <img src="{{asset('images/da_loc.png')}}">
+                    <img src="{{asset('images/da_loc.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Pangasinan </p>
@@ -217,7 +217,7 @@
 
             <div class="col-sm-3">
                 <a class="lightbox" href="#branch19">
-                    <img src="{{asset('images/da_loc.png')}}">
+                    <img src="{{asset('images/da_loc.png')}}">">
                 </a>
                 <div class="caption">
                     <p style="text-align: center; color:#ffbf00; font-size: 20px; "> Candon </p>
@@ -232,83 +232,83 @@
 
     <!--------------------- EACH LIGHTBOX --------------------------->
     <div class="lightbox-target" id="branch">
-        <img src="{{asset('images/da10.png"/>')}}">
+        <img src="{{asset('images/da10.png')}}">">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch1">
-        <img src="{{asset('images/da2.png')}}"/>
+        <img src="{{asset('images/da2.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch2">
-        <img src="{{asset('images/da11.png')}}"/>
+        <img src="{{asset('images/da11.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch3">
-        <img src="{{asset('images/da8.png')}}"/>
+        <img src="{{asset('images/da8.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch4">
-        <img src="{{asset('images/da9.png')}}"/>
+        <img src="{{asset('images/da9.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch5">
-        <img src="{{asset('images/da3.png')}}"/>
+        <img src="{{asset('images/da3.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch6">
-        <img src="{{asset('images/da4.png')}}"/>
+        <img src="{{asset('images/da4.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch7">
-        <img src="{{asset('images/da13.png')}}"/>
+        <img src="{{asset('images/da13.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch8">
-        <img src="{{asset('images/da1.png')}}"/>
+        <img src="{{asset('images/da1.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch9">
-        <img src="{{asset('images/da14.png')}}"/>
+        <img src="{{asset('images/da14.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch10">
-        <img src="{{asset('images/da15.png')}}"/>
+        <img src="{{asset('images/da15.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch11">
-        <img src="{{asset('images/da7.png')}}"/>
+        <img src="{{asset('images/da7.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch12">
-        <img src="{{asset('images/da5.png')}}"/>
+        <img src="{{asset('images/da5.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch13">
-        <img src="{{asset('images/da17.png')}}"/>
+        <img src="{{asset('images/da17.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch14">
-        <img src="{{asset('images/da6.png')}}"/>
+        <img src="{{asset('images/da6.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch15">
-        <img src="{{asset('images/da16.png')}}"/>
+        <img src="{{asset('images/da16.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch16">
-        <img src="{{asset('images/da12.png')}}"/>
+        <img src="{{asset('images/da12.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch17">
-        <img src="{{asset('images/da_loc.png')}}"/>
+        <img src="{{asset('images/da_loc.png')}}">
         <a class="lightbox-close" href="branches.blade.php"></a>
     </div>
     <div class="lightbox-target" id="branch18">
-        <img src="{{asset('images/da_loc.png')}}"/>
+        <img src="{{asset('images/da_loc.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
     <div class="lightbox-target" id="branch19">
-        <img src="{{asset('images/da_loc.png')}}"/>
+        <img src="{{asset('images/da_loc.png')}}">
         <a class="lightbox-close" href="#"></a>
     </div>
 @endsection
